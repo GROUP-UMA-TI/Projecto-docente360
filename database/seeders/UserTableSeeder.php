38596db1b2e1767aa58020a18fc96134481b116b
unknown <james.perezlima@gmail.com>
2026-01-09 11:36:38 -0500

    primer commit Projecto-docente360

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'lastname' => 'User',
+                'genero' => 'M',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('123456'),
+                'grado' => 'Ing',
+                'firma' => '',
+                'status' => 1,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
